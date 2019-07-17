34d1b61b1980c0406628b71d699f8a30d1ff5ec5
benjaminbartels <bbartels@gmail.com>
2019-07-17 07:20:45 -0600

    ui work

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -1,7 +1,38 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import manifestJSON from '../../public/manifest.json'
+import {
+  VApp,
+  VBtn,
+  VBtnToggle,
+  VCard,
+  VCheckbox,
+  VDivider,
+  VGrid,
+  VIcon,
+  VList,
+  VProgressLinear,
+  VTextField
+} from 'vuetify'
+
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  components: {
+    VApp,
+    VBtn,
+    VBtnToggle,
+    VCard,
+    VCheckbox,
+    VDivider,
+    VGrid,
+    VIcon,
+    VList,
+    VProgressLinear,
+    VTextField
+  },
+  theme: {
+    primary: manifestJSON.theme_color
+  }
 })
